5edce7d236f84b3b7de9e8eb9dd4494214db4e51
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2024-02-11 14:04:20 +0530

    Initial Commit

---
+++ learningiOS/learningiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningiOSApp.swift
+//  learningiOS
+//
+//  Created by Adarsh Shukla on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct learningiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
